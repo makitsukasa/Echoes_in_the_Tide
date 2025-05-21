972e4f68afab7a901389b1b1a0a6c0dc1b0b1eb9
makitsukasa <allium.fistulosum.39@gmail.com>
2025-05-22 02:15:16 +0900

    lets start refactoring

--- frontend/types/filebase.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@filebase/sdk' {
-  export class ObjectManager {
-    constructor(key: string, secret: string, options: { bucket: string });
-    upload(path: string, data: Buffer | Uint8Array, options?: any, metadata?: any): Promise<void>;
-    stat(path: string): Promise<any>;
-  }
-}
--- frontend/types/ipfs-only-hash.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'ipfs-only-hash' {
-  export function of(
-    data: Buffer | Uint8Array,
-    options?: { cidVersion?: number }
-  ): Promise<string>;
-}
